61776e2f62c460023b0446d230b3b25bcf41c041
natsu-anon <jarano93@gmail.com>
2024-02-22 16:38:23 -0800

    snippies

--- snippets/gdscript-mode/.yas-compiled-snippets.el
+++ snippets/gdscript-mode/.yas-compiled-snippets.el
@@ -2,17 +2,17 @@
 ;;; Snippet definitions:
 ;;;
 (yas-define-snippets 'gdscript-mode
-					 '(("v3z" "Vector3.ZERO$0" "Vector3.ZERO" nil nil nil "/home/jon/.emacs.d/snippets/gdscript-mode/v3z" nil nil)
-					   ("v2z" "Vector2.ZERO$0" "Vector2.ZERO" nil nil nil "/home/jon/.emacs.d/snippets/gdscript-mode/v2z" nil nil)
-					   ("_unhandled" "func _unhandled_input(_event: InputEvent) -> void:\n	${1:pass}\n" "unhandled_input" nil nil nil "/home/jon/.emacs.d/snippets/gdscript-mode/unhandled_input" nil nil)
-					   ("_ready" "func _ready() -> void:\n	${1:pass}" "ready" nil nil nil "/home/jon/.emacs.d/snippets/gdscript-mode/ready" nil nil)
-					   ("_process" "func _process(_delta: float) -> void:\n	${1:pass}\n" "process" nil nil nil "/home/jon/.emacs.d/snippets/gdscript-mode/process" nil nil)
-					   ("_physics" "func _physics_process(_delta: float) -> void:\n	${1:pass}\n" "physics process" nil nil nil "/home/jon/.emacs.d/snippets/gdscript-mode/physics_process" nil nil)
-					   ("_input" "func _input(_event: InputEvent) -> void:\n	${1:pass}\n" "input" nil nil nil "/home/jon/.emacs.d/snippets/gdscript-mode/input" nil nil)
-					   ("_init" "func _init() -> void:\n	${1:pass}" "init" nil nil nil "/home/jon/.emacs.d/snippets/gdscript-mode/init" nil nil)
-					   ("fn" "func ${1:func_name}(${2:}) -> ${3:void}:\n	$0" "fn" nil nil nil "/home/jon/.emacs.d/snippets/gdscript-mode/func" nil nil)
-					   ("v3" "Vector3$0" "Vector3" nil nil nil "/home/jon/.emacs.d/snippets/gdscript-mode/V3" nil nil)
-					   ("v2" "Vector2$0" "Vector2" nil nil nil "/home/jon/.emacs.d/snippets/gdscript-mode/V2" nil nil)))
+					 '(("v3z" "Vector3.ZERO$0" "Vector3.ZERO" nil nil nil "c:/Users/Jon/.emacs.d/snippets/gdscript-mode/v3z" nil nil)
+					   ("v2z" "Vector2.ZERO$0" "Vector2.ZERO" nil nil nil "c:/Users/Jon/.emacs.d/snippets/gdscript-mode/v2z" nil nil)
+					   ("_unhandled" "func _unhandled_input(_event: InputEvent) -> void:\n	${1:pass}\n" "unhandled_input" nil nil nil "c:/Users/Jon/.emacs.d/snippets/gdscript-mode/unhandled_input" nil nil)
+					   ("_ready" "func _ready() -> void:\n	${1:pass}" "ready" nil nil nil "c:/Users/Jon/.emacs.d/snippets/gdscript-mode/ready" nil nil)
+					   ("_process" "func _process(_delta: float) -> void:\n	${1:pass}\n" "process" nil nil nil "c:/Users/Jon/.emacs.d/snippets/gdscript-mode/process" nil nil)
+					   ("_physics" "func _physics_process(_delta: float) -> void:\n	${1:pass}\n" "physics process" nil nil nil "c:/Users/Jon/.emacs.d/snippets/gdscript-mode/physics_process" nil nil)
+					   ("_input" "func _input(_event: InputEvent) -> void:\n	${1:pass}\n" "input" nil nil nil "c:/Users/Jon/.emacs.d/snippets/gdscript-mode/input" nil nil)
+					   ("_init" "func _init(${1:}) -> void: # add arguments that so they can be passed into new()\n	${2:pass}" "init" nil nil nil "c:/Users/Jon/.emacs.d/snippets/gdscript-mode/init" nil nil)
+					   ("fn" "func ${1:func_name}(${2:}) -> ${3:void}:\n	$0" "fn" nil nil nil "c:/Users/Jon/.emacs.d/snippets/gdscript-mode/func" nil nil)
+					   ("v3" "Vector3$0" "Vector3" nil nil nil "c:/Users/Jon/.emacs.d/snippets/gdscript-mode/V3" nil nil)
+					   ("v2" "Vector2$0" "Vector2" nil nil nil "c:/Users/Jon/.emacs.d/snippets/gdscript-mode/V2" nil nil)))
 
 
-;;; Do not edit! File generated at Thu Jan  4 16:03:13 2024
+;;; Do not edit! File generated at Thu Feb 22 13:20:05 2024
