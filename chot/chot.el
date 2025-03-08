3206fade90f54a0c3a850d8dc21ea3eee754b2ac
natsu-anon <jarano93@gmail.com>
2025-03-07 16:08:46 -0800

    banger

--- chot/chot.el
+++ chot/chot.el
@@ -4,16 +4,17 @@
  (defvar chot-script "chot.py"
    "path to chot script to use, defaults to '~/.emacs.d/chot/chot.py'")
 
-(defun chot (&optional target port)
+(defun chot (&optional url port)
   "View a youtube livestream chat in a seperate buffer--NON-BLOCKING!!!"
-  (interactive"starget: \nnport: ")
+  (interactive"surl: \nnport: ")
   (message "chotting on port: %d" port)
   (get-buffer-create "*chot*")
   (display-buffer "*chot*")
+  (string-match "v=\\(.\\{11\\}\\)" url)
   (make-process
    :name "chot"
    :buffer (get-buffer "*chot*")
-   :command `(,python-executable ,chot-script  ,target ,(number-to-string port)))
+   :command `(,python-executable ,chot-script  ,(match-string 1 url) ,(number-to-string port)))
   (open-network-stream "chot-conn" (get-buffer "*chot*") "localhost" port :type 'plain))
 
 (provide 'chot)
